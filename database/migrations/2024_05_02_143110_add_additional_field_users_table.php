e35917028e31d75c1cb268d8551e9aa0d361893e
Rajvansh <106300079+steve-369@users.noreply.github.com>
2024-05-18 12:54:43 +0530

    migration update

--- database/migrations/2024_05_02_143110_add_additional_field_users_table.php
+++ database/migrations/2024_05_02_143110_add_additional_field_users_table.php
@@ -13,21 +13,21 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
 
-            $table->string('last_name')->after('name');
-            $table->string('gender')->after('last_name');
-            $table->date('dob')->default('2024-01-01')->after('gender');
-            $table->string('martial_status')->after('dob');
-            $table->string('no_of_children')->after('martial_status');
-            $table->string('area')->after('no_of_children');
-            $table->string('on_behalf')->after('area');
-            $table->string('on_behalf_name')->after('on_behalf')->default('');
-            $table->string('phone')->after('on_behalf_name');
-            $table->string('wp_no')->after('phone');
-            $table->string('degree')->after('wp_no');
-            $table->string('address')->after('degree');
-            $table->string('country')->after('address');
-            $table->string('state')->after('country');
-            $table->string('city')->after('state');
+            $table->string('last_name')->after('name')->nullable();
+            $table->string('gender')->after('last_name')->nullable();
+            $table->date('dob')->default('2024-01-01')->after('gender')->nullable();
+            $table->string('martial_status')->after('dob')->nullable();
+            $table->string('no_of_children')->after('martial_status')->nullable();
+            $table->string('area')->after('no_of_children')->nullable();
+            $table->string('on_behalf')->after('area')->nullable();
+            $table->string('on_behalf_name')->after('on_behalf')->default('')->nullable();
+            $table->string('phone')->after('on_behalf_name')->nullable();
+            $table->string('wp_no')->after('phone')->nullable();
+            $table->string('degree')->after('wp_no')->nullable();
+            $table->string('address')->after('degree')->nullable();
+            $table->string('country')->after('address')->nullable();
+            $table->string('state')->after('country')->nullable();
+            $table->string('city')->after('state')->nullable();
             // $table->string('height')->after('address');
             // $table->string('profession')->after('height');
             $table->string('is_admin')->after('city')->default(0);
