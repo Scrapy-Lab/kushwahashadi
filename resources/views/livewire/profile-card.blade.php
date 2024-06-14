7a6dccca94ea26000698a28ed12b0bd299f5a361
Rajvansh <106300079+steve-369@users.noreply.github.com>
2024-06-14 15:52:16 +0530

    null image done

--- resources/views/livewire/profile-card.blade.php
+++ resources/views/livewire/profile-card.blade.php
@@ -9,8 +9,15 @@
         @else
 
         @endif --}}
-    <img src="{{ asset('storage/profile_img/' . $user_details->user_image) }}" wire:ignore class="show-image"
-        alt="">
+    @if ($user_detail->gender == 1)
+        <img src="{{ asset('storage/profile_img/' . ($user_detail->user_image ?? 'default_male.jpg')) }}"
+            alt="">
+    @else
+        <img src="{{ asset('storage/profile_img/' . ($user_detail->user_image ?? 'default_female.jpg')) }}"
+            alt="">
+    @endif
+    {{-- <img src="{{ asset('storage/profile_img/' . $user_details->user_image) }}" wire:ignore class="show-image"
+        alt=""> --}}
     <form wire:submit="store_profile_image" wire:ignore>
         @csrf
         <input type="file" name="image" class="image">
